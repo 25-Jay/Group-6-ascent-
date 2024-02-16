dde1ad17a2a6824df7a086b9b92609e044606a3e
25-Jay <ang.ying.jie.jay@Gmail.com>
2024-02-16 13:20:39 +0800

    Added UI elements

---
+++ Ascent project/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public float timeRemaining = 0;
+    public bool timeisRunning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeisRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeisRunning)
+        {
+            if (timeRemaining >= 0)
+            {
+                timeRemaining += Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
+}
