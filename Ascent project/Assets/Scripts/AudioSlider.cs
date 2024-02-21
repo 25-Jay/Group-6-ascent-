b98d8ed6ec5a3628113d27be226afaa45e7fe193
05-kinwai <septiceyejack71@gmail.com>
2024-02-22 06:07:23 +0800

    Updated UI,Scripts
    Added Pause menu scripts, Audio slider, UI Assets, backgrounds, Audio

---
+++ Ascent project/Assets/Scripts/AudioSlider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSlider : MonoBehaviour
+{
+    [SerializeField] Slider audioslider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void Load()
+    {
+        audioslider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", audioslider.value);
+    }
+    public void ChangeVolume()
+    {
+        AudioListener.volume = audioslider.value;
+    }
+}
---
+++ Ascent project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(pauseMenu != null)
+            {
+                pauseMenu.SetActive(!pauseMenu.activeSelf);
+            }
+            {
+                if (pauseMenu.activeSelf)
+                {
+                    Time.timeScale = 0f;
+                }
+                else
+                {
+                    Time.timeScale = 1f;
+                }
+            }
+        }
+    }
+}
